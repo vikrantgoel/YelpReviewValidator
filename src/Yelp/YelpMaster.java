f01527741c36ad80dddc825a4ccb8de87f289237
Vikrant Goel <vgoel1@walmartlabs.com>
2016-07-09 04:56:46 -0700

    tip and review added to hive

--- src/Yelp/YelpMaster.java
+++ src/Yelp/YelpMaster.java
@@ -13,6 +13,14 @@ public class YelpMaster {
 	private static HiveClient hc;
 	private static JSONToTSV jt;
 	
+	public static void tipLoadToHive() throws ParseException, IOException{
+		jt.jsonToTsv("business_id,user_id,text,date,likes", "yelp_academic_dataset_tip.json", "yelp_academic_dataset_tip.txt", "\t");
+		hc.loadFromFileToTable(new File("yelp_academic_dataset_tip.txt").getAbsolutePath(), "tip", true);
+	}
+	public static void reviewLoadToHive() throws ParseException, IOException{
+		jt.jsonToTsv("business_id,user_id,stars,text,date,votes", "yelp_academic_dataset_review.json", "yelp_academic_dataset_review.txt", "\t");
+		hc.loadFromFileToTable(new File("yelp_academic_dataset_review.txt").getAbsolutePath(), "review", true);
+	}
 	public static void businessLoadToHive() throws ParseException, IOException{
 		jt.jsonToTsv("business_id,name,full_address,city, state,open,categories,review_count,stars,neighborhoods,longitude,latitude,attributes", "yelp_academic_dataset_business.json", "yelp_academic_dataset_business.txt", "\t");
 		hc.loadFromFileToTable(new File("yelp_academic_dataset_business.txt").getAbsolutePath(), "business", true);
@@ -27,6 +35,8 @@ public class YelpMaster {
 		hc = new HiveClient("yelp", true);
 		
 //		businessLoadToHive();
-		usersLoadToHive();
+//		usersLoadToHive();
+//		reviewLoadToHive();
+//		tipLoadToHive();
 	}
 }
